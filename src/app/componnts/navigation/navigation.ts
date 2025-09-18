30571336cc757d911882a9aba006b94cdd15b799
James Rock <hello@jamesrock.me>
2025-09-18 08:59:09 +0100

    updates

--- src/app/componnts/navigation/navigation.ts
+++ src/app/componnts/navigation/navigation.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { NgIcon, provideIcons } from '@ng-icons/core'; 
-import { boxHome } from '@ng-icons/boxicons/regular';
 
 @Component({
   selector: 'app-navigation',
-  imports: [RouterLink, NgIcon],
-  templateUrl: './navigation.html',
-  viewProviders: [provideIcons({boxHome})]
+  imports: [RouterLink],
+  templateUrl: './navigation.html'
 })
 export class Navigation {
 
