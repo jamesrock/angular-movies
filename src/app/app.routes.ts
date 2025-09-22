d430940e45e90ef2238565a5304604d61d525e69
James Rock <hello@jamesrock.me>
2025-09-22 12:30:15 +0100

    updates routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Coming } from './pages/coming/coming';
+import { Playing } from './pages/playing/playing';
 import { Genre } from './pages/genre/genre';
 import { Movie } from './pages/movie/movie';
 import { Person } from './pages/person/person';
-import { Playing } from './pages/playing/playing';
 import { Recs } from './pages/recs/recs';
 import { Genres } from './pages/genres/genres';
 
